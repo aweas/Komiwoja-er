2dad728c82e52a4a3f35db5560f153a8659fcc0c
wwydmanski <wwydmanski@gmail.com>
2017-05-20 01:32:55 +0200

    Algorithm benchmarking functions introduced

--- Komiwojażer/Komiwojażer.cpp
+++ Komiwojażer/Komiwojażer.cpp
@@ -13,18 +13,18 @@ using namespace std;
 double scoreGenes(vector<int> genes, double x[], double y[]);
 vector<int> randPermute();
 double scoreAll(vector<vector<int>> genes, double xLoc[], double yLoc[]);
-vector<vector<int>> evolve(vector<vector<int>> genes, double xLoc[], double yLoc[]);
+vector<vector<int>> evolve(vector<vector<int>> genes, double xLoc[], double yLoc[], int bestCopies, int twoCopies, int threeCopies, int restCopies, int crossRate, int mutationRate);
 vector<vector<int>> sort(vector<vector<int>> genes, vector<int> scores);
-vector<vector<int>> crossover(vector<vector<int>> genes);
-vector<vector<int>> mutate(vector<vector<int>> genes);
+vector<vector<int>> crossover(vector<vector<int>> genes, int rate);
+vector<vector<int>> mutate(vector<vector<int>> genes, int rate);
 vector<int> selectBest(vector<vector<int>> genes, double xLoc[], double yLoc[]);
 int findIndex(vector<int> mother, int value);
-
+int smallest(vector<int> vec);
 
 
 int main()
 {
-	srand(time(NULL));
+	srand(0);
 
 	fstream plik;
 	plik.open("dane.txt");
@@ -41,48 +41,87 @@ int main()
 	for (int i = 0; i < 100; i++)
 		genes.push_back(randPermute());
 
-	int average = 0;
-	int max=INT32_MAX;
 	vector<int> best;
 	
-	double limit = 1000;
+	double limit = 1;
 	int repetitions = 1;
 	vector<double> score;
 
-	for (int k = 0; k < repetitions; k++)
-	{
-		int tempMax=0;
-		max = INT32_MAX;
-		for (int i = 0; i < limit; i++)
-		{
-			genes = evolve(genes, xLoc, yLoc);
-			tempMax = scoreAll(genes, xLoc, yLoc);
-			if (tempMax < max)
-			{
-				max = tempMax;
-				best = selectBest(genes, xLoc, yLoc);
-			}
-			if(i%10==0)
-				cout << i << " " << tempMax << "    \r";
-		}
-		score.push_back(tempMax);
-		average += tempMax;
-		cout << "\r" << k;
-	}
-	average /= repetitions;
-	double variation=0;
-	for (int i = 0; i < score.size(); i++)
-		variation += (score[i] - average)*(score[i]-average);
-	
-	variation /= repetitions;
-	
-	cout << endl<<"Average score: "<<average << endl;
-	cout << "Variation: "<<variation << endl;
+	vector<int> bestCopies = { 0,0,0,10,30,50,15,30,45 }; //fill with best option
+	vector<int> twoCopies = { 0,0,0,0,0,0,10,20,30 };
+	vector<int> threeCopies = { 0,0,0,0,0,0,5,10,15 };
+	vector<int> restCopies = { 30,50,80,70,50,30,0,0,0 };
+	vector<int> crossRate = { 10,10,10,10,10,10,10,10,10, 5,20,30 }; //fill with best option
+	vector<int> mutationRate = { 10,10,10,10,10,10,10,10,10, 10,10,10, 5,20,30 };
 
-	cin.get();
+	vector<int> history;
+	vector<int> historyFar;
+
+	for (int exp = 0; exp < mutationRate.size(); exp++)
+	{
+		//Restart randomness seed
+		srand(0);
+		int average = 0;
+		for (int k = 0; k < repetitions; k++)
+		{
+			int tempMax = 0;
+
+			for (int i = 0; i < limit; i++)
+				genes = evolve(genes, xLoc, yLoc, bestCopies[exp], twoCopies[exp], threeCopies[exp], restCopies[exp], crossRate[exp], mutationRate[exp]);
+				
+			tempMax = scoreAll(genes, xLoc, yLoc);
+			score.push_back(tempMax);
+			average += tempMax;
+			printf("\rRepetition: %i \t", k);
+		}
+
+		//Statistics measuring
+		average /= repetitions;
+
+		double variation = 0;
+		for (int i = 0; i < score.size(); i++)
+			variation += (score[i] - average)*(score[i] - average);
+		variation /= repetitions;
+
+		printf("\r\t\t\n");
+		printf("Experiment: %i\nBest: %i\nVariation: %df\n", exp, average, variation);
+		score.clear();
+
+		history.push_back(average);
+		historyFar.push_back(average);
+
+		if ((exp+1) % 3 == 0)
+		{
+			int best = smallest(history);
+			printf("\nBest experiment out of three: %i, average: %i\n", best, history[best]);
+			history.clear();
+
+			if (exp == 8 || exp == 11)
+			{
+				int bestFar = smallest(historyFar);
+				printf("Best experiment so far: %i, average: %i\n", bestFar, historyFar[bestFar]);
+
+				for (int i = 0; i < 6 && exp==8; i++)
+				{
+					bestCopies.push_back(bestCopies[bestFar]);
+					twoCopies.push_back(twoCopies[bestFar]);
+					threeCopies.push_back(threeCopies[bestFar]);
+					restCopies.push_back(restCopies[bestFar]);
+				}
+				for (int i = 0; i < 3 && exp == 11; i++)
+				{
+					crossRate.push_back(bestCopies[bestFar]);
+				}
+			}
+		}
+
+		
+	}
+
+	std::cin.get();
 }
 
-vector<vector<int>> evolve(vector<vector<int>> genes, double xLoc[], double yLoc[])
+vector<vector<int>> evolve(vector<vector<int>> genes, double xLoc[], double yLoc[], int bestCopies, int twoCopies, int threeCopies, int restCopies, int crossRate, int mutationRate)
 {
 	vector<int> scores, j;
 
@@ -94,23 +133,23 @@ vector<vector<int>> evolve(vector<vector<int>> genes, double xLoc[], double yLoc
 
 	//Fill new generation with 5 times winner, 3 times 2nd place, 2 times 3rd place, 1 time next 80. Fill rest with rand
 	vector<vector<int>> newEvolution;
-	for (int i = 0; i < 50; i++)
+	for (int i = 0; i < bestCopies; i++)
 		newEvolution.push_back(genes[0]);
-	for (int i = 0; i < 30; i++)
+	for (int i = 0; i < twoCopies; i++)
 		newEvolution.push_back(genes[1]);
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < threeCopies; i++)
 		newEvolution.push_back(genes[2]);
-	//for (int i = 0; i < 70; i++)
-	//	newEvolution.push_back(genes[i]);
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < restCopies; i++)
+		newEvolution.push_back(genes[i]);
+	for (int i = 0; i < 100-bestCopies-twoCopies-threeCopies-restCopies; i++)
 		newEvolution.push_back(randPermute());
 
 	//Crossover
 	//Each 'speciman' becomes a father. Then each of his genes has 10% chance of being replaced by random mother's gene
-	newEvolution = crossover(newEvolution);
+	newEvolution = crossover(newEvolution, crossRate);
 
 	//Mutate
-	newEvolution = mutate(newEvolution);
+	newEvolution = mutate(newEvolution, mutationRate);
 
 	return newEvolution;
 }
@@ -132,7 +171,7 @@ vector<vector<int>> sort(vector<vector<int>> genes, vector<int> scores)
 	return genes;
 }
 
-vector<vector<int>> crossover(vector<vector<int>> genes)
+vector<vector<int>> crossover(vector<vector<int>> genes, int rate)
 {
 	for (int i = 1; i < 100; i++)
 	{
@@ -141,7 +180,7 @@ vector<vector<int>> crossover(vector<vector<int>> genes)
 
 		//1% chance that we insert mother's sequence instead of mothers
 		for (int j = 0; j < 100; j++)
-			if (rand() % 1000 < 10)
+			if (rand() % 1000 < rate)
 			{
 				/*int index = findIndex(father, mother[j]);*/
 				int index = findIndex(mother, father[j]);
@@ -154,11 +193,11 @@ vector<vector<int>> crossover(vector<vector<int>> genes)
 	return genes;
 }
 
-vector<vector<int>> mutate(vector<vector<int>> genes)
+vector<vector<int>> mutate(vector<vector<int>> genes, int rate)
 {
 	for (int j = 1; j<100; j++)
 		for (int i = 0; i < 100; i++)
-			if (rand() % 1000 < 10)
+			if (rand() % 1000 < rate)
 				swap(genes[j][rand()%100], genes[j][rand() % 100]);
 	return genes;
 }
@@ -228,4 +267,13 @@ vector<int> randPermute()
 	for (int i = 0; i < 100; i++)
 		swap(temp[rand() % 100], temp[rand() % 100]);
 	return temp;
+}
+
+int smallest(vector<int> vec)
+{
+	int min = vec[0];
+	for (int i = 1; i < vec.size(); i++)
+		if (vec[i] < min)
+			min = vec[i];
+	return findIndex(vec, min);
 }
